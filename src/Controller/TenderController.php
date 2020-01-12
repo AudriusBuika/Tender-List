4926698f57f58c95246fe26c7d55e463b08116f9
Audrius Buika <audrius.buika@gmail.com>
2020-01-13 00:48:37 +0200

    added delete tender action

--- src/Controller/TenderController.php
+++ src/Controller/TenderController.php
@@ -3,6 +3,8 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Tender;
 
 class TenderController extends AbstractController
 {
@@ -15,21 +17,59 @@ class TenderController extends AbstractController
         return $this->render('tender/tender.html.twig', [
         ]);
     }
+
+    /**
+     * Get tender record list
+     * 
+     * @Route("/list")
+     */
+    public function list()
+    {
+        $response = new JsonResponse();
+        $doctrine = $this->getDoctrine();
+
+        $tender = $doctrine->getRepository(Tender::class)->findTenders(20);
+
+        return $response->setData([
+                'success' => 1,
+                'tender'  => $tender,
+        ]);
+    }
     
     /**
+     * Update edit tender record 
+     * 
      * @Route("/update")
      */
     public function update()
     {
-
     }
 
     /**
-     * @Route("/delete")
+     * Delete tender record
+     * 
+     * @Route("/delete/{id}")
      */
-    public function delete()
+    public function delete(int $id)
     {
+        $response = new JsonResponse();
+        $doctrine = $this->getDoctrine();
+        $entityManager = $doctrine->getManager();
 
+        $tender = $doctrine->getRepository(Tender::class)->find($id);
+
+        if(!$tender) {
+            return $response->setData([
+                'success' => 0
+            ]);
+        }
+
+        $entityManager->remove($tender);
+        $entityManager->flush();
+
+        return $response->setData([
+            'success' => 1
+        ]);
     }
 
 }
