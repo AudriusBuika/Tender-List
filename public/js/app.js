186b9a6766b3c36f6456410ea3a8e2ab32597af1
Audrius Buika <audrius.buika@gmail.com>
2020-01-13 00:49:48 +0200

    added angular delete action

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,39 @@ app.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('[[').endSymbol(']]');
 });
 
-app.controller("tenderListController", ["$scope", function($scope) {
-    $scope.kazkas = "oho";
+app.controller("tenderListController", ["$scope","$http","$log", function($scope, $http, $log) {
+    
+    $scope.tenders = null;
+
+    var successCallBack = function(response) {
+        $scope.tenders = response.data.tender;
+    }
+    var failureCallBack = function(response) {
+        $scope.error = response.data;
+    }
+
+    // load list function
+    var loadData = function() {
+        $http({
+            method: 'GET',
+            url: '/list'
+        }).then(successCallBack, failureCallBack);
+    };
+
+    loadData(); // load data
+    
+    // Delete record function
+    $scope.Delete = function(id) {
+        $http({
+            method: 'GET',
+            url: '/delete/'+id
+        }).then(function(response) {
+            loadData();
+        });
+    };
+
+    $scope.Update = function(id) {
+        
+    }
+    
 }]);
