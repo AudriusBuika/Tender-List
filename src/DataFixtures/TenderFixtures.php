2c682e5db6c19530016c82c5bbaaeb81427e1b3a
Audrius Buika <audrius.buika@gmail.com>
2020-01-12 15:52:09 +0200

    added Tender Fixtures

---
+++ src/DataFixtures/TenderFixtures.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Tender;
+
+class TenderFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        
+        $faker = \Faker\Factory::create();
+
+        foreach(range(1,4000) as $x) {
+            $tender = new Tender();
+            $tender->setTitle($faker->sentence(mt_rand(1,3)));
+            $tender->setDescription($faker->sentence(mt_rand(20,60)));
+            $manager->persist($tender);
+        }
+
+        $manager->flush();
+    }
+}
